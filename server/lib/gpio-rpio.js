a6c2e96a0935c1e38d837def7bf5858528b96244
Bill McKessy <bill@192.168.0.100>
2021-09-16 16:08:15 +0200

    alpha release

---
+++ server/lib/gpio-rpio.js
@@ -0,0 +1,46 @@
+const gpio = require('rpio')
+
+const pins = [6,21,22,23,24]
+let error = false
+let setupComplete = false
+
+const pinStats = pins.map(pinNumber=>{
+  return {
+    doneSetup: false,
+    error: false,
+    pinNumber
+  }
+})
+
+function _gpio () {
+  function init () {
+    console.log('initializing pins')
+    pins.forEach((pin,pinIdx)=>{
+      console.log('setting up pin: ' + pinIdx)
+      gpio.open(pin, gpio.OUTPUT)
+    })
+  }
+  function doPins (pattern, step) {
+
+    pins.forEach((pin,pinIdx) => {
+      const channelIdx = pinIdx + 1
+      const channel = pattern.channels[channelIdx]
+      if(channel === undefined) {
+        console.log('undefined channel' + channelIdx)
+        return
+      }
+      const valueIndex = channel.steps.findIndex(o=>o.idx === step)
+      gpio.write(
+        pin, 
+        valueIndex === undefined ? gpio.LOW : gpio.HIGH
+      )
+    })
+  }
+  
+  return {
+    doPins,
+    init
+  }
+}
+
+module.exports = _gpio
