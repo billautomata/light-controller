2885f74ba59742047635d3fda25413fd0c1d711b
Bill McKessy <bill@192.168.0.100>
2021-09-28 11:10:17 +0200

    process song and process playlist working

--- server/lib/StateMachine/lib/Util/processSong.js
+++ server/lib/StateMachine/lib/Util/processSong.js
@@ -1,1 +1,24 @@
-module.exports = function processPattern () {}
+module.exports = function processSong () {
+  this.sequencer.currentStep += 1
+  this.sequencer.currentStep = this.sequencer.currentStep % this.config.songData.songLength
+
+  if (this.sequencer.currentStep === 0) {
+    sequencer.sequenceStarted_ms = Date.now()
+  }
+
+  console.log(['current step',this.sequencer.currentStep,this.config.songData.steps[this.sequencer.currentStep].join(',')].join('\t'))
+
+  const timeValue = this.config.songData.steps[this.sequencer.currentStep][0]
+  if (timeValue !== 0) {
+    this.sequencer.currentSpeed = timeValue
+  }
+
+  this.sequencer.nextActionTime = Date.now() + this.sequencer.currentSpeed
+
+  const sum = this.config.songData.patterns.map(o=>o.msLength).reduce((a,b)=>a+b)
+  const percentElapsed = (Date.now() - this.sequencer.sequenceStarted_ms) / sum
+
+  this.sendMessage('set-step', { value: this.sequencer.currentStep })
+  this.sendMessage('set-step-time', { value: percentElapsed })
+
+}
